cf19fc16ef25bd1dd3029aa41b7b258aab915bc9
zahid1k <mzk21443@gmail.com>
2024-12-28 23:46:21 +0300

    README added and last udpates with fixes 	modified:   code/MyCovertChannel.py 	modified:   code/README.md 	modified:   code/config.json

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -1,10 +1,8 @@
 from CovertChannelBase import CovertChannelBase
 
-import time
 import json
+import time
 import random
-import struct
-import socket
 
 import scapy.all as scapy
 import scapy.layers
@@ -13,60 +11,74 @@ import scapy.layers.inet
 import scapy.sendrecv
 import scapy.packet
 from scapy.all import sniff, IP, ICMP
+from scapy.packet import Packet
 
 
 class MyCovertChannel(CovertChannelBase):
     """
-    - You are not allowed to change the file name and class name.
-    - You can edit the class in any way you want (e.g. adding helper functions); however, there must be a "send" and a "receive" function, the covert channel will be triggered by calling these functions.
+    MyCovertChannel class implements a covert storage channel using protocol field manipulation.
+    - Encodes and decodes binary messages in IP header fields (TOS).
+    - Handles sending and receiving packets using Scapy.
     """
+
     def __init__(self):
         """
-        - You can edit __init__.
+        Initializes any required variables for encoding/decoding messages.
         """
-        # self.possible_values = [
-        #     "0011",  # 3
-        #     "0101",  # 5
-        #     "0110",  # 6
-        #     "1001",  # 9
-        #     "1010",  # 10
-        #     "1100",  # 12
-        # ]
-        # self.xor_res_to_bits = {
-        #     "0": [["0", "0"], ["1", "1"]],
-        #     "1": [["1", "0"], ["0", "1"]]
-        # }
-    
+        pass
+
     def generate_rand_hex(self, possible_values) -> str:
+        """
+        Selects a random value from a given list of possible values.
+        Args:
+            possible_values: List of valid hex values for encoding.
+        Returns:
+            A randomly selected hex value.
+        """
         selected_hex = possible_values[random.randint(0, len(possible_values)-1)]
         return selected_hex
 
     def encode_message(self, two_bits: str, possible_values, xor_res_to_bits):
-        """two_bits in ilk bit i 0 larin tuttuklarinin xor u olucak"""
+        """
+        Encodes 2 bits into an 8-bit TOS value.
+        Args:
+            two_bits: 2-bit binary string to encode.
+            possible_values: List of valid starting patterns.
+            xor_res_to_bits: Mapping for XOR results to bit pairs.
+        Returns:
+            Encoded binary string.
+        """
         selected_hex = self.generate_rand_hex(possible_values)
         a = selected_hex
         zero_second_digit = None
         one_second_digit = None
+
         for i in a:
             if i == "0":
                 if zero_second_digit is None:
-                    xx = xor_res_to_bits[two_bits[0]][random.randint(0,1)]
+                    xx = xor_res_to_bits[two_bits[0]][random.randint(0, 1)]
                     selected_hex += xx[0]
                     zero_second_digit = xx[1]
                 else:
                     selected_hex += zero_second_digit
-            else:  # i == "1"
+            else:
                 if one_second_digit is None:
-                    xx = xor_res_to_bits[two_bits[1]][random.randint(0,1)]
+                    xx = xor_res_to_bits[two_bits[1]][random.randint(0, 1)]
                     selected_hex += xx[0]
                     one_second_digit = xx[1]
                 else:
                     selected_hex += one_second_digit
-        print("two_bits:", two_bits, "\tselected_hex:", selected_hex)
+
         return selected_hex
 
     def decode_message(self, total_hash):
-        # print("total_hash:", total_hash)
+        """
+        Decodes a binary string extracted from TOS values.
+        Args:
+            total_hash: Concatenated binary string to decode.
+        Returns:
+            Decoded 2-bit binary string.
+        """
         decoded_hex = ""
 
         zero_first_digit = None
@@ -75,130 +87,77 @@ class MyCovertChannel(CovertChannelBase):
         zero_xor = None
         one_xor = None
 
-        for i in range(4):
-            first_ = total_hash[i]
-            second_ = total_hash[i+4]
-            if first_ == "0":
+        half_len = int(len(total_hash)/2)
+        assert half_len == 4
+
+        for i in range(half_len):
+            pattern_bit = total_hash[i]
+            corresponding_bit = total_hash[i + half_len]
+            if pattern_bit == "0":
                 if zero_first_digit is None:
-                    zero_first_digit = second_
+                    zero_first_digit = corresponding_bit
                 else:
-                    zero_xor = int(zero_first_digit) ^ int(second_)
-            else:  # i == "1"
+                    zero_xor = int(zero_first_digit) ^ int(corresponding_bit)
+            else:
                 if one_first_digit is None:
-                    one_first_digit = second_
+                    one_first_digit = corresponding_bit
                 else:
-                    one_xor = int(one_first_digit) ^ int(second_)
+                    one_xor = int(one_first_digit) ^ int(corresponding_bit)
         
         return str(zero_xor) + str(one_xor)
 
-    # def create_ip_packet(self, tos):
-    #     version = 4  # IPv4
-    #     ihl = 5      # Header length
-    #     tot_len = 20 # Total length (no payload)
-    #     id = 54321   # Identification
-    #     frag_off = 0 # Fragment offset
-    #     ttl = 64     # Time-to-live
-    #     protocol = 6 # TCP
-    #     src_ip = "192.168.1.1"  # Source IP
-    #     dst_ip = "192.168.1.2"  # Destination IP
-
-    #     # Create the IP header
-    #     ver_ihl = (version << 4) + ihl
-    #     header = scapy.packet.Packet('!BBHHHBBH4s4s',
-    #                         ver_ihl, tos, tot_len, id, frag_off,
-    #                         ttl, protocol, 0,  # Checksum is 0 for now
-    #                         socket.inet_aton(src_ip),
-    #                         socket.inet_aton(dst_ip))
-
-    #     return header
-
-    def send(self, log_file_name, parameter1, parameter2, 
-             possible_values, xor_res_to_bits, send_sleep_time_in_ms,
-             receiver_ip):  # parameter2 is the number of 0's in first 4 bits
+    def send(self, log_file_name, possible_values, xor_res_to_bits, send_sleep_time_in_ms,
+             sender_decoded_bit_length, receiver_ip):
         """
-        - In this function, you expected to create a random message (using function/s in CovertChannelBase), and send it to the receiver container. Entire sending operations should be handled in this function.
-        - After the implementation, please rewrite this comment part to explain your code basically.
+        Sends covert messages by encoding binary data into TOS fields of IP packets.
+        Args:
+            log_file_name: File name for logging the sent message.
+            possible_values: Valid binary patterns for encoding.
+            xor_res_to_bits: XOR mapping for encoding.
+            send_sleep_time_in_ms: Delay between packet transmissions.
+            sender_decoded_bit_length: Bits encoded per packet.
+            receiver_ip: Receiver's IP address.
         """
         binary_message = self.generate_random_binary_message_with_logging(log_file_name)
-
-        bps_calc_message = self.generate_random_binary_message(16,16)
-
-        binary_message = bps_calc_message
-
-        print(f"binary_message ({type(binary_message)}) :", binary_message)
-        print([self.convert_eight_bits_to_character(binary_message[x:x+8]) for x in range(0, len(binary_message), 8)])
-
-        start_time = time.time()
-        for index in range(0, len(binary_message), 2):
-
-            tos_ = self.encode_message(binary_message[index:index+2], possible_values, xor_res_to_bits)
-            print(tos_, end="\t")
+        for index in range(0, len(binary_message), sender_decoded_bit_length):
+            tos_ = self.encode_message(binary_message[index:index+sender_decoded_bit_length], 
+                                       possible_values, xor_res_to_bits)
             tos_dec = int(tos_, 2)
-
             ip_packet = scapy.layers.inet.IP(dst=receiver_ip, tos=tos_dec)
-
             icmp_request = scapy.layers.inet.ICMP()
             packet = ip_packet / icmp_request
+            super().send(packet)
+            time.sleep(send_sleep_time_in_ms / 1000)
 
-            super().send(packet, interface="eth0")
-
-            # scapy.sendrecv.send(packet)
-
-            time.sleep(send_sleep_time_in_ms/1000)
-        end_time = time.time()
-
-        print("TIME CONSUMED:", 128/(end_time - start_time))
-
-
-    def receive(self, parameter1, parameter2, parameter3, log_file_name,
+    def receive(self, log_file_name,
                 received_decoded_bit_length, received_encoded_bit_length,
                 sender_ip):
         """
-        - This function listens for ICMP packets in a continuous loop to extract covert data.
-        - It filters packets based on ICMP type and processes them to decode hidden information.
-        - Logs each packet's details to a specified log file.
+        Receives covert messages by decoding binary data from TOS fields of IP packets.
+        Args:
+            log_file_name: File name for logging the received message.
+            received_decoded_bit_length: Bits decoded per packet.
+            received_encoded_bit_length: Expected encoded bits per packet.
+            sender_ip: Sender's IP address.
         """
-
         self.log_message("", log_file_name)
         decoded_part = ""
-        count = 0
-
         res = ""
 
         while True:
-            # Capture a single packet with ICMP filter
             packet = sniff(filter="icmp", count=1)[0]
-            count += received_decoded_bit_length
-
             if scapy.layers.inet.IP in packet and scapy.layers.inet.ICMP in packet:
-
-                # Display packet details
-                # print(packet)
-                # print("***************")
-                
-                # Convert packet to JSON format and extract 'tos'
                 b = packet.json()
                 tos_ = json.loads(b)["payload"]["tos"]
-
                 decodd = str(bin(tos_)[received_decoded_bit_length:])
                 if len(decodd) < received_encoded_bit_length:
-                    decodd = "0"*(received_encoded_bit_length-len(decodd)) + decodd
-                print(decodd)
-                
+                    decodd = "0" * (received_encoded_bit_length - len(decodd)) + decodd
                 decoded_binary = self.decode_message(decodd)
                 decoded_part += decoded_binary
-
                 if len(decoded_part) % 8 == 0:
                     obtained_char = self.convert_eight_bits_to_character(decoded_part)
                     decoded_part = ""
-
-                    # print(obtained_char, end="")
-                    
                     res += obtained_char
-
                     if obtained_char == ".":
                         break
-        print()
-        # Log the packet data
         self.log_message(res, log_file_name)
-
--- code/README.md
+++ code/README.md
@@ -1,3 +1,71 @@
-# Replace with Your Covert Channel Name
+# Covert Storage Channel that exploits Protocol Field Manipulation using Type of Service field in IP
 
-Explain your study in detail as you share your work with the community in a public repository. Anyone should understand your project when read it without having a previous information about the homework.
+## Overview
+This project implements a **Covert Storage Channel** that exploits the **Type of Service (TOS)** field in the IP header for data transfer. The covert channel leverages unused bits in the TOS field to encode and transmit hidden messages between two endpoints.
+
+The implementation includes:
+1. **Message Encoding and Decoding:** The message is encoded using a custom bit manipulation scheme within the TOS field and decoded by the receiver.
+2. **Packet Transmission and Reception:** Messages are sent and received using ICMP packets.
+3. **Logging and Testing Framework:** The system logs sent and received messages for verification.
+
+## Encoding Scheme
+- Each 8-bit TOS field encodes **2 bits** of actual message data.
+- The first 4 bits determine the encoding pattern, containing **two 0s and two 1s**.
+- The XOR operation between pairs of bits determines the encoded message bits.
+- The XOR operation of the **0 bits pattern** gives first bit, **1 bits pattern** gives the second bit.
+- The encoded bits are extracted and reconstructed into characters by the receiver.
+
+### Encoding Example
+For a binary message '10':
+1. Generate an 8-bit encoded TOS value.
+2. Match bits based on the pattern and XOR them to encode '10'.
+3. Send this encoded value within the TOS field of an IP packet.
+
+## Implementation Details
+### Key Components
+- **MyCovertChannel.py**: Contains the main logic for encoding, decoding, sending, and receiving messages.
+- **CovertChannelBase.py**: Provides utility functions for binary conversion, logging, and sending packets.
+- **run.py**: Script to execute send and receive operations.
+- **config.json**: Configuration file to set parameters like IP addresses, encoding patterns, and logs.
+- **Makefile**: Automation script for running tests and verifying logs.
+
+### Measurement of Covert Channel Capacity
+The covert channel capacity is calculated based on the time taken to send and receive messages.
+
+**Capacity Formula:**
+```
+Capacity (bps) = Total Bits Sent / Transmission Time (seconds)
+```
+In our implementation:
+- Total bits sent = 128 bits.
+- Transmission time = ~8.01 seconds.
+- **Measured Capacity: 15.98 bits per second**.
+
+## How to Run the Code
+1. **Sender:**
+   ```bash
+   make send
+   ```
+2. **Receiver:**
+   ```bash
+   make receive
+   ```
+3. **Compare Logs:**
+   ```bash
+   make compare
+   ```
+4. **View Documentation:**
+   ```bash
+   make documentation
+   ```
+
+## Results
+- **Logs Comparison:** Logs generated at sender and receiver are compared to ensure message integrity.
+- **Capacity Measurement:** Covert channel capacity achieved: **15.98 bps**.
+
+## Limitations
+- The implementation relies on the TOS field, which may be overwritten or stripped by network devices.
+- The capacity may vary based on network latency and system performance.
+
+## Conclusion
+This project successfully demonstrates a covert storage channel exploiting the TOS field in IP headers. It achieves a capacity of approximately **15.98 bits per second** and provides a framework for further exploration of covert channels in networking protocols.
